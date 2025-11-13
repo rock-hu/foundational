36582555095e49d179ed2b0f4eb44421398f6055
rock.c.c.hu@gmail.com <rock.c.c.hu@gmail.com>
2025-11-14 00:28:29 +0800

    feat: .rnstorybook/stories

--- .rnstorybook/main.ts
+++ .rnstorybook/main.ts
@@ -2,7 +2,10 @@ import type { StorybookConfig } from '@storybook/react-native';
 
 const main: StorybookConfig = {
   stories: ['./stories/**/*.stories.?(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
+  addons: ['@storybook/addon-ondevice-notes',
+    '@storybook/addon-ondevice-controls',
+    '@storybook/addon-ondevice-backgrounds',
+    '@storybook/addon-ondevice-actions', '@storybook/addon-react-native-web'],
 };
 
 export default main;
